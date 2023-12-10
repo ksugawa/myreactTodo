657bbb11015827799fe4c9c1842536d9a77a6bd5
k-sugawa <sugawakumiko@gmail.com>
2023-12-10 23:33:22 +0900

    inputの文字記録

--- myapp/src/components/TodoInput.js
+++ myapp/src/components/TodoInput.js
@@ -3,24 +3,40 @@ import { v4 as uuidv4 } from "uuid";
 
 export const TodoInput = ({ todos, setTodos }) => {
 
-const TodoInput = ({ todoinput, setTodos}) => {
+  const [inputText, setInputText] = useState("")
     
-    const todoNameRef = useRef();
+  const handleSubmit = (e) => {
+    e.preventDefault(); 
 
-    const handleAddTodo = () => {
-      const name = todoNameRef.current.value;
-      if (name === "") return;
       setTodos((prevTodos) => [
-        ...prevTodos,
-        { id: uuidv4(), name: name, completed: false },
-      ]);
-      todoNameRef.current.value = null;
-    };
+      ...todos,
+      {
+        id: uuidv4(),
+        text: inputText
+      }
+    ]);
+    setInputText("")    
+  }
+
+  const handleChange = (e) => {
+    setInputText(e.target.value);
+  }
+
+  // const handleAddTodo = () => {
+  //   const name = todoNameRef.current.value;
+  //   if (name === "") return;
+  //   setTodos((prevTodos) => [
+  //     ...prevTodos,
+  //     { id: uuidv4(), name: name, completed: false },
+  //   ]);
+  //   todoNameRef.current.value = null;
+  // };
 
     return (
     <>
     <form onSubmit={handleSubmit}>
         <div className="todoinput-line">
+        <input type="text" onChange={handleChange} value={inputText} placeholder="新しいTodoを追加" />
         <button className="todo-addBtn">
         <img src="/assets/images/icon-add.svg" alt="add-icon"/>
         </button>
@@ -29,7 +45,5 @@ const TodoInput = ({ todoinput, setTodos}) => {
     </>
     );
 };
-
-
 
 export default TodoInput;
